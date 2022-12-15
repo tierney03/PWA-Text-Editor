d5fcc50f53abbf905abdb16835961dc43ceb9aef
tierney03 <tierney_03@mac.com>
2022-12-15 14:16:29 -0600

    Fixed Issue w/classmate over why the dist was not created

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -62,7 +62,7 @@ module.exports = () => {
             options: {
               presets: ["@babel/preset-env"],
               plugins: [
-                "@babel/plugin-proposal=-object-rest-spread",
+                "@babel/plugin-proposal-object-rest-spread",
                 "@babel/transform-runtime",
               ],
             },
